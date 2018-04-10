042d93cd6132ab6bb3306fcec3e58aae303f6983
Unoblueboy <Nathan20467@gmail.com>
2018-04-10 22:04:12 +0100

    Added more comments to explain what everything does

--- domain_colouring.py
+++ domain_colouring.py
@@ -1,44 +1,75 @@
+'''Here is the module Doc-string
+'''
+import colorsys as cs
 import numpy as np
 from matplotlib import pyplot as plt
-# from colorsys import hls_to_rgb
-import colorsys as cs
+from skimage.filters import sobel
 
-from skimage import feature
-from skimage.filters import gaussian, sobel
 
 class Domain(object):
     ''' A class to represent the domain a function should be plotted in
     Created so the Function Plot function initialisation variables
     Don't get too messy
-    
+
     Typical use:
-    
+
         d = Domain(-1,2,-3,4)       # A domain with corners -1+4i and 2-3i
     '''
 
     def __init__(self, min_re, max_re, min_im, max_im):
         '''Initialises the class with the maximum and minimum, real and
-        imaginary values to be used'''
+        imaginary values to be used
+
+        Parameters:
+            min_re:     float
+                the minimum real value to be considered in the domain
+            max_re:     float
+                the maximum real value to be considered in the domain
+            min_im:     float
+                the minimum imaginary value to be considered in the domain
+            max_im:     float
+                the maximum imaginary value to be considered in the domain
+        '''
         self.min_re = min_re
         self.max_re = max_re
         self.min_im = min_im
         self.max_im = max_im
 
+    def set_upper_left(self, z: complex):
+        '''function to define the upper left corner of the domain
+
+        Parameters:
+            z:      Comlex
+                A point in the complex plane to define the upper left corner of the domain
+        '''
+        self.min_re = z.real
+        self.max_im = z.imag
+
+    def set_lower_right(self, z: complex):
+        '''function to define the lower_right corner of the domain
+
+        Parameters:
+            z:      Comlex
+                A point in the complex plane to define the lower right corner of the domain
+        '''
+        self.max_re = z.real
+        self.min_im = z.imag
+
+
 class FunctionPlot():
     '''A class dedicated to the domain colouring of a complex function
-    
+
     The function once initialised will go through the process of creating a domain plot
-    as well as grid plot and blend the two to end up with the end result. The domain plot 
+    as well as grid plot and blend the two to end up with the end result. The domain plot
     created depends on the domain colouring algorithm used, which is defined by the user,
     and a grid plot is made if the user so decides it.
-    
+
     Typical use:
-    
+
         d = Domain(-3,3,-3,3)
-        FunctionPlot(lambda z: z**2, d_c, d, 512, 512)  # Here d_c is some domain colouring algorithm
-        
+        FunctionPlot(lambda z: z**2, dc, d, 512, 512)  # Here dc is some domain colouring algorithm
     '''
-    def __init__(self, func, domain_colouring, domain, width, height, grid = False):
+    def __init__(self, func, domain_colouring, domain, width, height, grid=False):
         '''A function to intialise FunctionPlot as a well as show the function
 
         Parameters:
@@ -70,69 +101,106 @@ class FunctionPlot():
             self.res = self.color_plot
 
     def _gen_complex_plane(self):
+        '''A function to create the plane on which the function will be applied
+        '''
         re = np.linspace(self.domain.min_re, self.domain.max_re, self.w)
         im = np.linspace(self.domain.min_im, self.domain.max_im, self.h)
         mesh_re, mesh_im = np.meshgrid(re, im)
-        complex_plane = mesh_re + 1j*mesh_im
+        complex_plane = mesh_re + 1j * mesh_im
         complex_plane = np.flip(complex_plane, 0)
         return complex_plane
-        
+
     def color(self, function_plane):
-        rgb = np.empty(list(function_plane.shape)+[4])
+        ''' Domain colour the plane based on the function used
+
+        Parameters:
+            function_plane:     NDArray
+                A 2D Array representing the value each point on the plane takes
+                under the transformation of the function
+        '''
+        rgb = np.empty(list(function_plane.shape) + [4])
 
         for i in range(0, self.h):
             for j in range(0, self.w):
-                rgb[i,j] = self.colouring(function_plane[i,j])
-        
+                rgb[i, j] = self.colouring(function_plane[i, j])
         return rgb
-    
+
     def grid_color(self, function_plane):
+        '''find the points that will be mapped onto a grid after transformation
+
+        Parameters:
+            function_plane:     NDArray
+                A 2D Array representing the value each point on the plane takes
+                under the transformation of the function
+        '''
         # Considering points that are MAPPED ONTO A GRID
         # Not points that are mapped from a grid
         bw_checkered = np.empty(list(function_plane.shape))
 
         for i in range(0, self.h):
             for j in range(0, self.w):
-                real_int = np.ceil(function_plane[i,j].imag)
-                imag_int = np.ceil(function_plane[i,j].real)
-                if (real_int - imag_int) % 2 == 1: # If one is even and the other odd
-                    bw_checkered[i,j] = 0
+                real_int = np.ceil(function_plane[i, j].imag)
+                imag_int = np.ceil(function_plane[i, j].real)
+                if (real_int - imag_int) % 2 == 1:  # If one is even and the other odd
+                    bw_checkered[i, j] = 0
                 else:
-                    bw_checkered[i,j] = 1
-        
+                    bw_checkered[i, j] = 1
+
         bw_checkered = sobel(bw_checkered)
 
-        bw_grid = np.empty(list(bw_checkered.shape)+[4])
+        bw_grid = np.empty(list(bw_checkered.shape) + [4])
         for i in range(0, self.h):
             for j in range(0, self.w):
-                if bw_checkered[i,j]==0:
-                    bw_grid[i,j] = [1,1,1,0]
+                if bw_checkered[i, j] == 0:
+                    bw_grid[i, j] = [1, 1, 1, 0]
                 else:
-                    bw_grid[i,j] = [0,0,0,1]
+                    bw_grid[i, j] = [0, 0, 0, 1]
         return bw_grid
 
     def blend(self, color_plot, grid_plot, weight):
+        '''Blend two plots together
+
+        Parameters:
+            color_plot:     NDArray
+                A 2D Array representing the domain colouring of the plane
+            grid_plot:      NDArray
+                A 2D Array representing the grid to be superimposed onto the
+                domain colouring plane
+            weight:         float
+                Represents how dark the grid lines should be on the graph
+                0 - None
+                1 - Full Black
+        '''
+
         result = np.zeros(color_plot.shape)
         for i in range(0, self.h):
             for j in range(0, self.w):
-                if grid_plot[i,j,3]==0:
-                    result[i,j] = color_plot[i,j]
+                if grid_plot[i, j, 3] == 0:
+                    result[i, j] = color_plot[i, j]
                 else:
-                    result[i,j] = color_plot[i,j]*(1-weight)
-                    result[i,j,3] = 1
+                    result[i, j] = color_plot[i, j] * (1 - weight)
+                    result[i, j, 3] = 1
         return result
 
     def plot(self):
+        '''A function to plot the function over the domain
+        '''
         plt.axis('off')
         plt.imshow(self.res, interpolation='nearest')
         plt.show()
-    
+
     def save(self, filename=None):
-        domain_width = self.domain.max_re-self.domain.min_re
-        domain_height = self.domain.max_im-self.domain.min_im
-        fig = plt.figure(figsize=(domain_width, 
-                                    domain_height))
-        ax1 = fig.add_subplot(1,1,1)
+        '''A function to save the plot of the function over the domain
+
+        Parameters:
+            filename:   string
+                The filename the image should be saved as
+        '''
+        domain_width = self.domain.max_re - self.domain.min_re
+        domain_height = self.domain.max_im - self.domain.min_im
+        fig = plt.figure(figsize=(domain_width,
+                                  domain_height))
+        ax1 = fig.add_subplot(1, 1, 1)
         ax1.imshow(self.res, interpolation='nearest')
         ax1.axis('off')
         extent = ax1.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
@@ -140,47 +208,49 @@ class FunctionPlot():
             fig.savefig(filename, bbox_inches=extent)
         else:
             fig.savefig('temp.png', bbox_inches=extent)
-        # plt.axis('off')
-        # plt.imshow(self.res, interpolation='nearest')
-        # if filename:
-        #     plt.savefig(filename)
-        # else:
-        #     plt.savefig('temp.png')
-        
 
 
 def domain_colouring(x):
-    # needs to return rgba with all values in interval [0,1]
+    '''A function defining how the function should be domain coloured
+
+    Parameters:
+        x:  complex
+            A value for which to determine the colouring
+
+    Returns:
+        A length 4 list representing RGBA. Each member is in the interval [0,1]
+    '''
     a = 0.5
-    hue = (0.5 + (np.pi + np.angle(x))/(2*np.pi)) % 1
-    value = a + (np.log(abs(x))/np.log(2) % 1)*(1-a)
+    hue = (0.5 + (np.pi + np.angle(x)) / (2 * np.pi)) % 1
+    value = a + (np.log(abs(x)) / np.log(2) % 1) * (1 - a)
     saturation = 1
     rgb_tuple = cs.hsv_to_rgb(hue, saturation, value)
-    return [rgb_tuple[0],rgb_tuple[1],rgb_tuple[2],1]
+    return [rgb_tuple[0], rgb_tuple[1], rgb_tuple[2], 1]
+
 
 if __name__ == '__main__':
-    # Cool looking seed values
-    # 2, 52, 94, 124396, 123456789, 123454321
+    ''' Cool looking seed values
+    2, 52, 94, 124396, 123456789, 123454321'''
     from function_generator import FunctionGenerator as FG
-    from scipy.special import gamma
     unary_ops = {
-                "sin" : lambda x : np.sin(x),
-                "cos" : lambda x : np.cos(x),
-                "tan" : lambda x : np.tan(x),
-                "sinh" : lambda x : np.sinh(x),
-                "cosh" : lambda x : np.cosh(x),
-                "tanh" : lambda x : np.tanh(x),
-                "repr" : lambda x : 1/x,
-            }
+        "sin": lambda x: np.sin(x),
+        "cos": lambda x: np.cos(x),
+        "tan": lambda x: np.tan(x),
+        "sinh": lambda x: np.sinh(x),
+        "cosh": lambda x: np.cosh(x),
+        "tanh": lambda x: np.tanh(x),
+        "repr": lambda x: 1 / x,
+    }
     f_gen = FG(unary_op=unary_ops)
     f1 = f_gen.generate_function(rand_seed=2)
+
     def f(z):
         ans = f1(z)
         if np.isfinite(ans):
             return ans
         else:
             return 0
-    d = Domain(-3,3,-3,3)
+    d = Domain(-3, 3, -3, 3)
     f_p = FunctionPlot(f, domain_colouring, d, 512, 512, grid=False)
     f_p.save()
     f_p.plot()
--- function_generator.py
+++ function_generator.py
@@ -1,52 +1,87 @@
-from math import (sin, cos, tan, 
-                 sinh, cosh, tanh)
+'''This is the module doc-string
+'''
+from math import (sin, cos, tan,
+                  sinh, cosh, tanh)
 from random import randint, sample, randrange, seed
 
+
 class FunctionGenerator(object):
-    def __init__(self, binary_op = {}, unary_op = {}):
+    '''A class to generate functions based on 2 sets. 1 containing binary
+    operations (taking 2 arguments), another containing unary operations
+    (taking 1 argument)
+
+    Typical Use:
+        >>> fg = FunctionGenerator() # Uses default sets for operations
+        >>> fg = FunctionGenerator(binary_op = {"sum": lambda x, y: x + y},
+                                   unary_op  = {"repr": lambda x: 1 / x})
+    '''
+    def __init__(self, binary_op={}, unary_op={}):
+        '''A function to initialise the class by defining the binary and
+        unary operations can be used as well as the number of them
+
+        Parameters:
+            binary_op:      dictionary
+                A dictionary of binary operations with their names
+            unary_op:      dictionary
+                A dictionary of unary operations with their names
+        '''
         if len(binary_op) == 0:
             binary_op = {
-                "add" : lambda x,y : x+y,
-                "sub" : lambda x,y : x-y,
-                "mul" : lambda x,y : x*y,
-                "div" : lambda x,y : x/y,
+                "add": lambda x, y: x + y,
+                "sub": lambda x, y: x - y,
+                "mul": lambda x, y: x * y,
+                "div": lambda x, y: x / y,
             }
         self.binary_ops = binary_op
         if len(unary_op) == 0:
             unary_op = {
-                "sin" : lambda x : sin(x),
-                "cos" : lambda x : cos(x),
-                "tan" : lambda x : tan(x),
-                "sinh" : lambda x : sinh(x),
-                "cosh" : lambda x : cosh(x),
-                "tanh" : lambda x : tanh(x),
-                "repr" : lambda x : 1/x,
+                "sin": lambda x: sin(x),
+                "cos": lambda x: cos(x),
+                "tan": lambda x: tan(x),
+                "sinh": lambda x: sinh(x),
+                "cosh": lambda x: cosh(x),
+                "tanh": lambda x: tanh(x),
+                "repr": lambda x: 1 / x,
             }
         self.unary_ops = unary_op
         self.num_bin_ops = len(self.binary_ops)
         self.num_una_ops = len(self.unary_ops)
-        self.total_ops = self.num_bin_ops+self.num_una_ops
+        self.total_ops = self.num_bin_ops + self.num_una_ops
         self.current_function = None
-    
-    def generate_function(self,  iteration_depth = 10, rand_seed = None):
+
+    def generate_function(self, iteration_depth=10, rand_seed=None):
+        '''A function to generate a function by repeated composition of
+        randomly chosen functions.
+
+        Parameters:
+            iteration_depth:    int
+                The number of times functions are composed on top of themselves
+            rand_seed:          int
+                The seed which is used to choose random functions. If it is None
+                then refer to the random module to find the seed which is used
+        '''
         if rand_seed:
             seed(rand_seed)
-        functions = (lambda z : z,)
+        functions = (lambda z: z,)
         for _ in range(iteration_depth):
             func = functions[-1]
-            num = randint(1,self.total_ops)
+            num = randint(1, self.total_ops)
             if num <= self.num_bin_ops:
-                bin_op_key = sample(self.binary_ops.keys(),1)[0]
+                bin_op_key = sample(self.binary_ops.keys(), 1)[0]
                 bin_op = self.binary_ops[bin_op_key]
-                val = randrange(-10,10)
-                if randint(1,2) == 1:
-                    g = lambda z, op=bin_op, f=func : op(f(z), val)
+                val = randrange(-10, 10)
+                if randint(1, 2) == 1:
+                    def g(z, op=bin_op, f=func):
+                        return op(f(z), val)
                 else:
-                    g = lambda z, op=bin_op, f=func : op(val, f(z))
+                    def g(z, op=bin_op, f=func):
+                        return op(val, f(z))
             else:
-                una_op_key = sample(self.unary_ops.keys(),1)[0]
+                una_op_key = sample(self.unary_ops.keys(), 1)[0]
                 una_op = self.unary_ops[una_op_key]
-                g = lambda z, op=una_op, f=func : op(f(z))
+
+                def g(z, op=una_op, f=func):
+                        return op(f(z))
             functions = functions.__add__((g,))
         self.current_function = functions[-1]
         return self.current_function
@@ -57,18 +92,16 @@ if __name__ == '__main__':
     from matplotlib import pyplot as plt
 
     unary_ops = {
-                "sin" : lambda x : np.sin(x),
-                "cos" : lambda x : np.cos(x),
-                "tan" : lambda x : np.tan(x),
-                "sinh" : lambda x : np.sinh(x),
-                "cosh" : lambda x : np.cosh(x),
-                "tanh" : lambda x : np.tanh(x),
-                "repr" : lambda x : np.divide(np.ones(x.shape),x),
-            }
+        "sin": lambda x: np.sin(x),
+        "cos": lambda x: np.cos(x),
+        "tan": lambda x: np.tan(x),
+        "sinh": lambda x: np.sinh(x),
+        "cosh": lambda x: np.cosh(x),
+        "tanh": lambda x: np.tanh(x),
+        "repr": lambda x: np.divide(np.ones(x.shape), x),
+    }
     f_gen = FunctionGenerator(unary_op=unary_ops)
     f1 = f_gen.generate_function()
-    xs = np.linspace(-3,3, 500)
-    plt.plot(xs,f1(xs))
+    xs = np.linspace(-3, 3, 500)
+    plt.plot(xs, f1(xs))
     plt.show()
-
-
